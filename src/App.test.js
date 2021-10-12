7f5adab7b2676d1944ea98a99a9d06f1c05810c5
Maximiliano Perchik <maxoperchik@gmail.com>
2021-10-11 22:10:22 -0300

    Segundo cambio

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/maxo/i);
+  const linkElement = screen.getByText(/juancito/i);
   expect(linkElement).toBeInTheDocument();
 });
