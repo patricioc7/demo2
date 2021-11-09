e1b28b373f677e14a01a7b1cce8da913c8d7330e
Patricio Córdoba <patricio.cordoba@mercadolibre.com>
2021-11-09 19:16:01 -0300

    pruebas de CI

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Primer cambio pipeline completo
+          Tp de ASO
         </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
